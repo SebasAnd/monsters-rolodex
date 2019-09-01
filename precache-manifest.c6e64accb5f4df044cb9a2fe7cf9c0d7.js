a69b39650ce985b9df89596c5deac069d7bd52b1
sebasand <andres.alejandro.cortes@correounivalle.edu.co>
2019-09-01 18:14:45 -0500

    Updates

---
+++ precache-manifest.c6e64accb5f4df044cb9a2fe7cf9c0d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a0d429570cb3edbcd1f128214aab9c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c2785130994c3748a98",
+    "url": "/monsters-rolodex/static/css/main.8da05efc.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "3c2785130994c3748a98",
+    "url": "/monsters-rolodex/static/js/main.747a7ed5.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
